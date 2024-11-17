64897783e900bac1b842cb63e87e9f163396e6d7
0Aniket Mishra <0aniketmishra@gmail.com>
2024-11-17 11:44:17 +0530

    Changed settings for normal domains.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: 'http://b3o33bdrhiuwehsr3fuqphmkxyzjnddvnzxj43wdpzjhsj3o67vw5hid.onion',
+    origin: '*',
      methods: 'GET, POST, OPTIONS',
       allowedHeaders: ['DNT', 'User-Agent', 'X-Requested-With', 'If-Modified-Since', 'Cache-Control', 'Content-Type'], credentials: true }));
 
